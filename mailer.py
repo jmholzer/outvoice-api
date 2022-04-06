30bb70843c2a0e5752b34ff38bfaa98393778e48
jmholzer <jannic.holzer@gmail.com>
2022-04-06 21:51:06 +0200

    Add full MIME multipart email construction with attachments.

--- mailer.py
+++ mailer.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Optional
 import boto3
 from botocore.exceptions import ClientError
 import json
@@ -6,6 +6,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 from utility import generate_absolute_path
+import os
 
 class EmailManager():
     """
@@ -23,9 +24,12 @@ class EmailManager():
         # Set object values with function calls
         self.ses_client = self.init_ses_client()
         # Read the name of the company controlling the outvoice instance
-        self.sender = self.init_sender(self)
+        self.sender = self.init_sender()
 
     def init_ses_client(self):
+        """
+        Returns an interface to SES.
+        """
         return boto3.client('ses', region_name="eu-central-1")
 
     def init_sender(self) -> None:
@@ -38,46 +42,108 @@ class EmailManager():
         with open(company_file_path) as company_file:
             return json.load(company_file)
 
-    def read_body(self) -> str:
+    def read_fields(self) -> str:
         """
-        Reads the html body from a file with fixed, predetermined location.
+        Reads the html body, subject and sender from a file with fixed, 
+        predetermined location into a dictionary.
         """
-        html_file_path = generate_absolute_path("/resources/email/body.html")
-        with open(html_file_path, "r") as html_file:
-            return html_file.read()
+        fields = {
+            "body": "",
+            "subject": "",
+            "sender": "",
+        }
+        for field in fields:
+            file_path = generate_absolute_path(f"/resources/email/{field}")
+            with open(file_path, "r") as file:
+                fields[field] = file.read()
+        return fields
 
-    def format_body(self, html_body: str, invoice_meta: dict) -> str:
+    def format_body(self, fields: Dict[str, str], invoice_meta: dict) -> None:
         """
-        Formats the supplied email html body with values specific to the 
+        Formats the supplied email html body with values specific to the
+        invoice being generated.
 
         Arguments:
-        html_body -- a string containing the html body of the email.
+        fields -- a dict containing the contents of the email.
         invoice_meta -- a dict containing the meta data on the invoice
             necessary to address the client.
         """
-        html_body.format(
+        fields["body"] = fields["body"].format(
             first_name=invoice_meta["first_name"],
             sender=self.sender["company_name"],
-            invoice_date=invoice_meta["invoice_date"],
+            invoice_date=invoice_meta["invoice_date"]
         )
-        return html_body
 
-    def construct_email(self, invoice_file_path: str):
+    def format_subject(self, fields: Dict[str, str]) -> None:
+        """
+        Formats the supplied email subject line with values specific to the
+        invoice being generated.
+
+        Arguments:
+        fields -- a dict containing the contents of the email.
+        """
+        fields["subject"] = fields["subject"].format(
+            sender=self.sender["company_name"]
+        )
+
+    def format_sender(self, fields: Dict[str, str]) -> None:
+        """
+        Formats the supplied email sender line with values specific to the
+        invoice being generated.
+
+        Arguments:
+        fields -- a dict containing the contents of the email.
+        """
+        fields["sender"] = fields["sender"].format(
+            sender=self.sender["company_name"],
+            email=self.sender["email"]
+        )
+
+    def construct_email_meta(self, message: MIMEMultipart, fields: dict) -> None:
+        """
+        Adds the 'subject', 'to' and 'from' metadata to a MIME multipart email
+        message.
+
+        Arguments:
+        message -- the MIME multipart message to add metadata to.
+        fields -- a dictionary containing the metadata.
+        """
+        pass
+
+    def construct_email(self, invoice_meta: dict, invoice_file_path: str):
         """
         Creates a multipart MIME message with a subject, html message body and
         attached invoice, ready to be sent by EmailManager.send_email.
 
         Arguments:
+        invoice_meta: meta data on the invoice being generated.
         invoice_file_path: absolute path of the invoice to be attached.
         """
+        fields = self.read_fields()
+        self.format_body(fields, invoice_meta)
+        self.format_subject(fields)
+        self.format_sender(fields)
         message = MIMEMultipart("mixed")
-        #message["Subject"] = 
+        message["Subject"] = fields["subject"]
+        message["From"] = fields["sender"]
+        message["To"] = invoice_meta["email_address"]
+        body = MIMEMultipart("alternative")
+        body.attach(MIMEText(fields["body"], "html", "utf-8"))
+        message.attach(body)
+        attachment = MIMEApplication(open(invoice_file_path, 'rb').read())
+        attachment.add_header(
+            'Content-Disposition',
+            'attachment',
+            filename=os.path.basename(invoice_file_path)+".pdf"
+        )
+        message.attach(attachment)
+        return message
 
     def send_email(
             self,
             recipient: str,
-            cc_recipient: str,
-            message: MIMEMultipart
+            message: MIMEMultipart,
+            cc_recipient: Optional[str] = ""
         ) -> bool:
         """
         Sends a MIME multipart email with a subject, html message body and
@@ -91,7 +157,7 @@ class EmailManager():
         """
         try:
             response = self.ses_client.send_raw_email(
-                Source=self.sender.email,
+                Source=self.sender["email"],
                 Destinations=[
                     recipient
                 ],
@@ -102,6 +168,5 @@ class EmailManager():
             if "MessageId" in response:
                 return True
         except ClientError as e:
+            print(e)
             return False
-    
-    
