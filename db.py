a6ae1efddf0cb9b1868b91b446423666e86a022f
jmholzer <jannic.holzer@gmail.com>
2022-04-10 21:25:36 +0200

    Add improved type hints and doc strings.

--- db.py
+++ db.py
@@ -6,15 +6,34 @@ from typing import Tuple, List
 
 class SqliteConnector():
     """
-    Class managing the connection 
+    Class managing a connection to the SQLite instance.
+
+    Attributes:
+        db_name -- the name of the SQLite database file to connect to.
+        con -- the sqlite3 object representing the connection to the database.
+    
+    Methods:
+        __init__
+        open_db
+        close_db
+        search_address
+        enter_address
+        remove_address
     """
 
     def __init__(self, db_name):
+        """
+        Initialise a new instance of SqliteConnector.
+        """
         self.db_name = db_name
         self.con = self.open_db()
         self.con.row_factory = Row
 
     def open_db(self):
+        """
+        Opens a connection to the SQLite3 database file pointed to by
+        the db_name attribute.
+        """
         application_path = os.path.dirname(os.path.realpath(__file__))
         return sqlite3.connect(
             application_path
@@ -25,9 +44,21 @@ class SqliteConnector():
         )
 
     def close_db(self):
+        """
+        Closes the connection to the SQLite3 database opened when the object
+        of this class was instantiated.
+        """
         self.con.close()
 
     def search_address(self, first_name: str, last_name: str) -> List[Row]:
+        """
+        Returns a list of sqlite3 Row objects containing information on each
+        row in the 'address' table with a matching first_name and last_name field.
+
+        Arguments:
+            first_name -- the first name of the client whose address to search for.
+            last_name -- the last name of the client whose address to search for.
+        """
         cur = self.con.cursor()
         result = cur.execute(
             """
@@ -47,6 +78,12 @@ class SqliteConnector():
         return list(result)
     
     def enter_address(self, row: Tuple[str]) -> None:
+        """
+        Enters a new row into the address table.
+        
+        Arguments:
+            row -- a tuple containing the data that will fill the new row.
+        """
         cur = self.con.cursor()
         cur.execute(
             """
@@ -60,6 +97,13 @@ class SqliteConnector():
         self.con.commit()
 
     def remove_address(self, row: Tuple[str]) -> None:
+        """
+        Removes a row from the address table, returns a dict containing the success
+        state of the operation.
+
+        Arguments:
+            row -- the row of data to remove.
+        """
         cur = self.con.cursor()
         cur.execute("select count(*) from address")
         row_count_pre = cur.fetchone()[0]
